018cfed589e47acef7a6e865f8cf651ff8f9cc7a
hjelstromboli <Brandon.Hjelstrom@gmail.com>
2024-04-24 15:01:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
 name='limelightlib-python',
-version='0.9.2',
+version='0.9.3',
 url='https://limelightvision.io',
 author='Brandon Hjelstrom',
 author_email='brandon@limelightvision.io',
 description='Built to interface with any Limelight Smart Camera',
+long_description=long_description,
+long_description_content_type="text/markdown",
 py_modules=["limelight", "limelightresults"],
 package_dir={"": "limelightlib-python"},
 classifiers=[
