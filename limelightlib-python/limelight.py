7850f429c6eaf5b9ec8fc112eef9baeeedbd5c16
Brandon <Brandon.Hjelstrom@gmail.com>
2024-04-30 19:57:06 -0700

    2024.6 updates

--- limelightlib-python/limelight.py
+++ limelightlib-python/limelight.py
@@ -64,31 +64,149 @@ class Limelight:
 
     def get_results(self):
         return requests.get(f"{self.base_url}/results").json()
-        
-    def capture_snapshot(self, snapname):
-        headers = {'snapname': snapname}
-        return requests.get(f"{self.base_url}/capturesnapshot", headers=headers).content
+    
+    def get_status(self):
+        response = requests.get(f"{self.base_url}/status")
+        if response.ok:
+            return response.json()
+        else:
+            return None
+    
+    # Get hardware characteristics
+    def hw_report(self):
+        return requests.get(f"{self.base_url}/hwreport").json()
+
+    # Force the camera to reload the current pipeline and all pipeline resources
+    def reload_pipeline(self):
+        return requests.post(f"{self.base_url}/reload-pipeline")
+
+    # Get default pipeline
+    def get_pipeline_default(self):
+        response = requests.get(f"{self.base_url}/pipeline-default")
+        if response.ok:
+            return response.json()
+        else:
+            return None
+
+    # Get pipeline[n] from the camera
+    def get_pipeline_atindex(self, index):
+        params = {'index': index}
+        response = requests.get(f"{self.base_url}/pipeline-atindex", params=params)
+        if response.ok:
+            return response.json()
+        else:
+            return None
+
+    # Switch to a different pipeline
+    def pipeline_switch(self, index):
+        params = {'index': index}
+        return requests.post(f"{self.base_url}/pipeline-switch", params=params)
+
+    # List of snapscriptpro names
+    def get_snapscript_names(self):
+        return requests.get(f"{self.base_url}/getsnapscriptnames").json()
+
+    def capture_snapshot(self, snapname=""):
+        params = {'snapname': snapname}
+        return requests.post(f"{self.base_url}/capturesnapshot", params=params)
 
     def upload_snapshot(self, snapname, image_path):
-        headers = {'snapname': snapname}
+        params = {'snapname': snapname}
         with open(image_path, 'rb') as image_file:
             files = {'file': image_file}
-            return requests.post(f"{self.base_url}/uploadsnapshot", headers=headers, files=files)
+            return requests.post(f"{self.base_url}/uploadsnapshot", params=params, files=files)
 
     def snapshot_manifest(self):
         return requests.get(f"{self.base_url}/snapshotmanifest").json()
 
     def delete_snapshots(self):
-        return requests.get(f"{self.base_url}/deletesnapshots")
+        return requests.delete(f"{self.base_url}/delete-snapshots")
 
-    def upload_neural_network(self, nn_type, file_path):
-        headers = {'type': nn_type}
+
+    def delete_snapshot(self, snapname):
+        params = {'snapname': snapname}
+        return requests.delete(f"{self.base_url}/delete-snapshot",params=params)
+
+    # Accepts Json with one or more settings updates. Set "flush" to 1 to save these settings to disk
+    def update_pipeline(self, profile_json, flush=None):
+        headers = {'Content-Type': 'application/json'}
+        params = {}
+        if flush is not None:
+            params['flush'] = flush
+        print(profile_json)
+        response = requests.post(f"{self.base_url}/update-pipeline", headers=headers, params=params, data=profile_json)
+        if response.status_code == 400:
+            # Check if the response is JSON-formatted
+            try:
+                error_details = response.json()  # Parse JSON to get error message
+                print("Error:", error_details)
+            except ValueError:
+                # If response is not JSON, print raw text
+                print("Error:", response.text)
+        return response
+
+
+    def update_python_inputs(self, inputs):
+        data = json.dumps(inputs)
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(f"{self.base_url}/update-pythoninputs", headers=headers, data=data)
+        return response
+
+    def update_robot_orientation(self, orientation_data):
+        data = json.dumps(orientation_data) 
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(f"{self.base_url}/update-robotorientation", headers=headers, data=data)
+        return response
+
+
+    def upload_pipeline(self, profile_json, index=None):
+        headers = {'Content-Type': 'application/json'}
+        params = {}
+        if index is not None:
+            params['index'] = index
+
+        response = requests.post(f"{self.base_url}/upload-pipeline", headers=headers, params=params, data=profile_json)
+        return response
+
+    def upload_fieldmap(self, fieldmap_json, index=None):
+        headers = {'Content-Type': 'application/json'}
+        params = {}
+        if index is not None:
+            params['index'] = index
+
+        response = requests.post(f"{self.base_url}/upload-fieldmap", headers=headers, params=params, data=fieldmap_json)
+        return response
+
+    def upload_python(self, pythonstring, index=None):
+        headers = {'Content-Type': 'text/plain'}
+        params = {}
+        if index is not None:
+            params['index'] = index
+
+        response = requests.post(f"{self.base_url}/upload-python", headers=headers, params=params, data=profile_json)
+        return response
+
+
+    def upload_neural_network(self, nn_type, file_path, index=None):
+        params = {'type': nn_type}
+        if index is not None:
+            params['index'] = index
+
         with open(file_path, 'rb') as nn_file:
-            files = {'file': nn_file}
-            return requests.post(f"{self.base_url}/uploadnn", headers=headers, files=files)
+            headers = {'Content-Type': 'application/octet-stream'}
+            file_contents = nn_file.read()
+            return requests.post(f"{self.base_url}/upload-nn", params=params, headers=headers, data=file_contents)
 
-    def hw_report(self):
-        return requests.get(f"{self.base_url}/hwreport").json()
+    def upload_neural_network_labels(self, nn_type, file_path, index=None):
+        params = {'type': nn_type}
+        if index is not None:
+            params['index'] = index
+
+        with open(file_path, 'rb') as nn_file:
+            file_contents = nn_file.read()
+            return requests.post(f"{self.base_url}/upload-nnlabels", params=params, headers=headers, data=file_contents)
+
+
 
     def cal_default(self):
         return requests.get(f"{self.base_url}/cal-default").json()
@@ -101,9 +219,6 @@ class Limelight:
 
     def cal_latest(self):
         return requests.get(f"{self.base_url}/cal-latest").json()
-
-    def hwreport(self):
-        return requests.get(f"{self.base_url}/hwreport").json()
 
     def update_cal_eeprom(self, cal_data):
         return requests.post(f"{self.base_url}/cal-eeprom", data=cal_data)
@@ -120,12 +235,7 @@ class Limelight:
     def delete_cal_file(self):
         return requests.delete(f"{self.base_url}/cal-file")
 
-    def get_status(self):
-        response = requests.get(f"{self.base_url}/status")
-        if response.ok:
-            return response.json()
-        else:
-            return None
+   
 
     def get_name(self):
         status = self.get_status()
